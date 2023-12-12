b25536f02d67e9bff3ab0565cc7009742b9be641
Satwik Gupta <satwikgupta1801@gmail.com>
2023-12-12 17:02:45 +0530

    upload

---
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+import { useState, useEffect} from "react"
+
+
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({})
+    let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    useEffect(() => {
+        fetch(url)
+        .then(response => response.json())
+        .then(response => setData(response[currency]))
+        console.log(data)
+
+    },[currency])
+    return data
+}
+
+export default useCurrencyInfo
